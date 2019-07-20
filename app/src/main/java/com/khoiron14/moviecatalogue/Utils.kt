09035dc646d8d6ec1e8ee1e2f4fc3b460af5fc83
Khoiron14 <muh.khoiron14@gmail.com>
2019-07-20 17:03:18 +0700

    add util file

---
+++ app/src/main/java/com/khoiron14/moviecatalogue/Utils.kt
@@ -0,0 +1,14 @@
+package com.khoiron14.moviecatalogue
+
+import android.view.View
+
+/**
+ * Created by khoiron14 on 7/20/2019.
+ */
+fun View.visible() {
+    visibility = View.VISIBLE
+}
+
+fun View.gone() {
+    visibility = View.GONE
+}
